eda6d30eed1c2d182d2f05df1d475819c847c078
RebeccaHomABK <109548240+RebeccaHomABK@users.noreply.github.com>
2022-08-10 17:44:57 -0700

    client(s) can send messages to server

--- ChatApplication/main.cpp
+++ ChatApplication/main.cpp
@@ -1,12 +1,26 @@
 #include <enet/enet.h>
 
 #include <iostream>
+#include <string>
 #include <thread>
 #include <chrono>
 
 ENetAddress address;
 ENetHost* server = nullptr;
 ENetHost* client = nullptr;
+
+struct Chat
+{
+    std::string m_name;
+    std::string m_content;
+    bool m_lock = false;
+    
+    Chat(std::string name, std::string content)
+        : m_name(name), m_content(content)
+    {};
+
+    std::string Message() { return m_name + ": " + m_content; }
+};
 
 //  Creates the chat server
 bool CreateServer()
@@ -58,6 +72,28 @@ int StartChat()
     return UserInput;
 }
 
+//  Sends a message
+void MessageSend(Chat* chat)
+{
+    std::string content = "";
+    while (1)
+    {
+        std::getline(std::cin, content);
+        chat->m_content = content;
+        chat->m_lock = true;
+        content = "";
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+}
+
+//  Sends a packet
+void PacketSend(std::string message, ENetHost* host = client)
+{
+    ENetPacket* packet = enet_packet_create(message.c_str(), message.length() + 1, ENET_PACKET_FLAG_RELIABLE);
+    enet_host_broadcast(host, 0, packet);
+    enet_host_flush(host);
+}
+
 //  Chat server functionality
 void ChatServer()
 {
@@ -73,34 +109,16 @@ void ChatServer()
                     << event.peer->address.host
                     << ":" << event.peer->address.port
                     << std::endl;
-                event.peer->data = (void*)("Client information");
 
-                {
-                    ENetPacket* packet = enet_packet_create("hello",
-                        strlen("hello") + 1,
-                        ENET_PACKET_FLAG_RELIABLE);
-                    enet_host_broadcast(server, 0, packet);
-                    //enet_peer_send(event.peer, 0, packet);
-
-                    //enet_host_service();
-                    enet_host_flush(server);
-                }
                 break;
             case ENET_EVENT_TYPE_RECEIVE:
-                std::cout << "A packet of length "
-                    << event.packet->dataLength << std::endl
-                    << "containing " << (char*)event.packet->data
-                    << std::endl;
-                //<< "was received from " << (char*)event.peer->data
-                //<< " on channel " << event.channelID << std::endl;
-            /* Clean up the packet now that we're done using it. */
+                std::cout << (char*)event.packet->data << std::endl;
                 enet_packet_destroy(event.packet);
 
                 break;
 
             case ENET_EVENT_TYPE_DISCONNECT:
                 std::cout << (char*)event.peer->data << "disconnected." << std::endl;
-                /* Reset the peer's client information. */
                 event.peer->data = NULL;
             }
         }
@@ -109,6 +127,16 @@ void ChatServer()
 
 void ChatClient()
 {
+    //  Gets the user's name.
+    Chat* chat = new Chat("", "");
+    std::cout << "Username: ";
+    std::cin.ignore(999, '\n');
+    std::getline(std::cin, chat->m_name);
+
+    system("cls");
+    std::cout << "Welcome, " << chat->m_name << "!" << std::endl;
+
+    //  Connect to the client.
     ENetAddress address;
     ENetEvent event;
     ENetPeer* peer;
@@ -125,10 +153,19 @@ void ChatClient()
         exit(EXIT_FAILURE);
     }
 
-    if (enet_host_service(client, &event, 5000) > 0 &&
+    if ((enet_host_service(client, &event, 1000) > 0) &&
         event.type == ENET_EVENT_TYPE_CONNECT)
     {
         std::cout << "Connection to " << host << ":" << address.port << " succeeded." << std::endl;
+
+        //  Sends a message to the entire chat room that the user has entered.
+        std::string welcome = chat->m_name + " has entered the chat.\n";
+        PacketSend(welcome);
+
+        //  Give instructions on what to do
+        std::cout << "Type in the chat below and hit enter to send. Type \\q to quit." << std::endl;
+        std::thread ChatThread(MessageSend, chat);
+        ChatThread.detach();
     }
     else
     {
@@ -138,30 +175,29 @@ void ChatClient()
 
     while (1)
     {
+        if (chat->m_lock)
+        {
+            if ((chat->m_content[0] == '\\') && (chat->m_content[1] == 'q'))
+            {
+                chat->m_content = chat->m_name + " has left the chat.\n";
+                PacketSend(chat->m_content, client);
+                delete chat;
+                return;
+            }
+
+            PacketSend(chat->Message(), client);
+            chat->m_lock = false;
+        }
+
         ENetEvent event;
 
-        while (enet_host_service(client, &event, 1000) > 0)
+        while (enet_host_service(client, &event, 500) > 0)
         {
             switch (event.type)
             {
             case ENET_EVENT_TYPE_RECEIVE:
-                std::cout << "A packet of length "
-                    << event.packet->dataLength << std::endl
-                    << "containing " << (char*)event.packet->data
-                    << std::endl;
-
+                std::cout << (char*)event.packet->data << std::endl;
                 enet_packet_destroy(event.packet);
-
-                {
-                    ENetPacket* packet = enet_packet_create("hi",
-                        strlen("hi") + 1,
-                        ENET_PACKET_FLAG_RELIABLE);
-
-                    enet_host_broadcast(client, 0, packet);
-                    //enet_peer_send(event.peer, 0, packet);
-
-                    enet_host_flush(client);
-                }
             }
         }
     }
@@ -170,6 +206,7 @@ void ChatClient()
 int main(int argc, char** argv)
 {
     int UserInput = StartChat();
+    system("cls");
     
     if (UserInput == 1)
     {
